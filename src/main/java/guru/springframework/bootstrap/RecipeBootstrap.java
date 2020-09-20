f92d41cbc07af7e902b6b2a3c0abbf8acbedd82c
andres.fleitas <andres.fleitas@smartmatic.com>
2020-09-19 22:21:17 -0500

    adding webflux

--- src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
+++ src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
@@ -41,6 +41,7 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
         loadUom();
         recipeRepository.saveAll(getRecipes());
         log.debug("Loading Bootstrap Data");
+
     }
 
     private void loadCategories(){
